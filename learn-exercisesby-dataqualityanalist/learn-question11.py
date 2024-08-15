6b19d500bbe019df78af5d7730e108e869c6a0e3
cspoojary <chaitanyaspoojary17@gmail.com>
2024-08-15 21:52:38 +0530

    15-08

--- learn-exercisesby-dataqualityanalist/learn-question11.py
+++ learn-exercisesby-dataqualityanalist/learn-question11.py
@@ -1,11 +1,10 @@
-
+# 11. Write a Python program to perform the following operations: 
+#     a) Use a for loop to print first 10 even numbers. 
 for i in range(20):
     if i % 2 ==0:
         print(i)
-
-x={1,2,3,4,5}
+#     b) Use a for loop to calculate the sum of the first 5 positive integers
 sum=0
-for n in x:
-    sum += n
-    print(sum)
-
+for i in range(5):
+    sum += i
+print("The sum of the first positive number",sum)
